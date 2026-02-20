228671665033d79d3034e7b88490ad1c2da4ff1f
Haseeb Khan <imhaseebkhan86@gmail.com>
2026-02-20 19:27:43 +0530

    multiple VM with Count variable

--- remotebackend.tf
+++ remotebackend.tf
@@ -4,7 +4,7 @@ terraform {
     resource_group_name  = "haseeb"
     storage_account_name = "haseebstorage08061986"
     container_name       = "myremotebackend"
-    key                  = "uat.terraform.tfstate"
+    key                  = "test.terraform.tfstate"
     use_azuread_auth     = true
   }
 }
--- vm/virtualmachin.tf
+++ vm/virtualmachin.tf
@@ -1,24 +1,64 @@
 
+
+data "azurerm_resource_group" "myrg" {
+  name = "test-rg"
+}
+
+output "id" {
+  value = data.azurerm_resource_group.myrg.id
+}
+
+
+
 resource "azurerm_virtual_network" "vnet" {
-  name                = "haseeb-${var.vnet}"
+  name                = var.vm
   address_space       = ["10.0.0.0/16"]
-  location            = var.rg_location
-  resource_group_name = var.rg_name
+  location            = data.azurerm_resource_group.myrg.location
+  resource_group_name = data.azurerm_resource_group.myrg.name
+
+  #depends_on = [ var.rg_name ]
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "internal"
-  resource_group_name  = var.rg_name
+  resource_group_name  = data.azurerm_resource_group.myrg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 
   depends_on = [ azurerm_virtual_network.vnet ]
 }
 
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = data.azurerm_resource_group.myrg.location
+  resource_group_name = data.azurerm_resource_group.myrg.name
+
+  security_rule {
+    name                       = "ssh_allow"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
+
+
+resource "azurerm_subnet_network_security_group_association" "nsglink" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+
 resource "azurerm_network_interface" "nic" {
-  name                = "haseeb-${var.nic}"
-  location            = var.rg_location
-  resource_group_name = var.rg_name
+  name                ="my-nic"
+  location            = data.azurerm_resource_group.myrg.location
+  resource_group_name = data.azurerm_resource_group.myrg.name
 
   ip_configuration {
     name                          = "internal"
@@ -26,26 +66,28 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id = azurerm_public_ip.pip.id
   }
-
-  depends_on = [ azurerm_subnet.subnet ]
+  #depends_on = [var.rg_name]   
+ 
   
 }
   
   
 
 resource "azurerm_public_ip" "pip" {
-  name                = var.pip
-  resource_group_name = var.rg_name
-  location            = var.rg_location
+  name                = "my-pip"
+  resource_group_name = data.azurerm_resource_group.myrg.name
+  location            = azurerm_resource_group.myrg.location
   sku                 = "Standard"  
   allocation_method   = "Static"
+
+  #depends_on = [ var.rg_name ]
 
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "haseeb-${var.vm}"
-  resource_group_name = var.rg_name
-  location            = var.rg_location
+  name                = "vm-001"
+  resource_group_name = data.azurerm_resource_group.myrg.name
+  location            = data.azurerm_resource_group.myrg.location
   size                = "Standard_D2ds_v4"
   admin_username      = "adminuser"
   admin_password      = "Password1234!"
@@ -65,4 +107,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+
+
+
+
+  
+
 }
